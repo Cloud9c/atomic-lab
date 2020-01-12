2240a576184ee02c7f3ab071a8575850b37452c5
Cloud9c <kire5030@gmail.com>
2020-01-12 16:30:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 var hoverElement, animate;
 
 window.addEventListener('load', (event) => {
-	if (localStorage[0]){
-		var slot = localStorage.getItem("slot").split(";");
-		var main = localStorage.getItem("main").split("`");
+	if (localStorage.slot){
+		var slot = localStorage.slot.split(";");
+		var main = localStorage.main.split("`");
 
 		for (var i = 0; i < 9; i++)
 			document.getElementsByClassName("slot")[i].innerHTML = slot[i];
@@ -399,52 +399,3 @@ onbeforeunload = function (e) {
 	localStorage.setItem("slot", slot);
 	localStorage.setItem("main", main);
 };
-
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-const PRECACHE_URLS = [
-  "index.html",
-  "main.css",
-  "script.js"
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
