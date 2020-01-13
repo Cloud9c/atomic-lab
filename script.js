2d9e4efbbbf4a2d6984d5c06b6bf315137efa923
Cloud9c <kire5030@gmail.com>
2020-01-12 21:57:22 -0500

    d

--- script.js
+++ script.js
@@ -400,50 +400,8 @@ onbeforeunload = function (e) {
 	localStorage.setItem("main", main);
 };
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/atomic-lab/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-self.addEventListener("install", function(evt) {
-	console.log("The service worker is being installed.");
-	evt.waitUntil(precache());
-});
-
-self.addEventListener("fetch", function(evt) {
-	console.log("The service worker is serving the asset.");
-	evt.respondWith(fromCache(evt.request));
-	evt.waitUntil(update(evt.request));
-});
-
-function precache() {
-  return caches.open("cache-and-update").then(function (cache) {
-    return cache.addAll([
-      './index.html',
-      './assets'
-    ]);
-  });
-}
-
-function fromCache(request) {
-  return caches.open("cache-and-update").then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-function update(request) {
-  return caches.open("cache-and-update").then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceworker.js', {
+    scope: '/'
   });
 }
