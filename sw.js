d3e1bf2413aa013601411482d2e7aba774d5d65a
Cloud9c <kire5030@gmail.com>
2020-01-12 23:58:16 -0500

    f

--- sw.js
+++ sw.js
@@ -4,10 +4,25 @@ self.addEventListener("install", function(evt) {
   evt.waitUntil(precache());
 });
 
-self.addEventListener("fetch", function(evt) {
-  console.log("The service worker is serving the asset.");
-  evt.respondWith(fromCache(evt.request));
-  evt.waitUntil(update(evt.request));
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.method === 'GET') {
+    evt.respondWith(
+      caches.match(evt.request)
+      .then((cached) => {
+        var networked = fetch(evt.request)
+          .then((response) => {
+            let cacheCopy = response.clone()
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(evt.request, cacheCopy))
+            return response;
+          })
+          .catch(() => caches.match(offlinePage));
+        return cached || networked;
+      })
+    )
+    evt.waitUntil(update(evt.request));
+  }
+  return;
 });
 
 function precache() {
