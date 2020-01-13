f235b215a2df9b00718b247bd78884b03ea6a659
Cloud9c <kire5030@gmail.com>
2020-01-12 21:38:34 -0500

    21ee

--- script.js
+++ script.js
@@ -400,7 +400,17 @@ onbeforeunload = function (e) {
 	localStorage.setItem("main", main);
 };
 
-var CACHE = "cache-and-update";
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 self.addEventListener("install", function(evt) {
 	console.log("The service worker is being installed.");
