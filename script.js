40f07d981b8b40f6f10e13f42568e07673b586d7
Cloud9c <kire5030@gmail.com>
2020-01-12 23:44:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -401,5 +401,11 @@ onbeforeunload = function (e) {
 };
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register("sw.js");
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
