65c8ac9463bd70d4e97e466562c14efe0abe36e1
Cloud9c <kire5030@gmail.com>
2020-01-16 20:39:27 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,22 +20,15 @@ self.addEventListener("install", function(evt) {
 
 self.addEventListener("fetch", function(evt) {
   console.log("The service worker is serving the asset.");
-  evt.respondWith(fromCache(evt.request));
-  evt.waitUntil(update(evt.request));
-});
-
-function fromCache(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return cache.match(request).then(function (matching) {
+  evt.respondWith(return caches.open(cacheName).then(function (cache) {
+    return cache.match(evt.request).then(function (matching) {
       return matching || Promise.reject("no-match");
     });
   });
-}
 
-function update(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
+  evt.waitUntil(caches.open(cacheName).then(function (cache) {
+    return fetch(evt.request).then(function (response) {
+      return cache.put(evt.request, response);
     });
   });
-}
+});
